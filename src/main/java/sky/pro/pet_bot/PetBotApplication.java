f510a47693be52a99bd97eb0b2c26ca171ba5748
Yuvis <yurii_s@bigmir.net>
2022-05-14 22:45:53 +0300

    init commit

---
+++ src/main/java/sky/pro/pet_bot/PetBotApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.pet_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sky/pro/pet_bot/PetBotApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.pet_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
